a03483d74340b19f9fb60bb52c1177e00eae2dea
ThaiTai <bttai2309@gmail.com>
2021-06-29 11:20:40 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using BigSchool.Models;
+using System;
+using Microsoft.Build.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public string Lecturer { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
